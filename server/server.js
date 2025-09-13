60e45a7463420728249609b0e794b1f27ae338f5
AdityaVerma810 <adicode810@gmail.com>
2025-09-14 02:16:08 +0530

    adding backend folder with initial files

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import https from "http";
+
+// create express app and HTTP server
+
+const app=express();
+const server=https.createServer(app);
+
+//Middleware
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req,res)=> res.send("server is live"));
+
+const PORT =process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log("Server is running on PORT :"+ PORT));
