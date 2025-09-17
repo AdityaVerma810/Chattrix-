0fe93ae8dc741884ae2034b71915b777220cb623
AdityaVerma810 <adicode810@gmail.com>
2025-09-18 00:49:08 +0530

    added server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import https from "http";
+import { connectDB } from "./lib/db.js";
 
 // create express app and HTTP server
 
@@ -13,6 +14,10 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use("/api/status", (req,res)=> res.send("server is live"));
+
+//connect to mongodb
+
+await connectDB();
 
 const PORT =process.env.PORT || 5000;
 
