94dfe036f13cdfb41c4cb1b977bf04172591f4f9
AdityaVerma810 <adicode810@gmail.com>
2025-10-09 02:18:09 +0530

    Socket.io functionalities in server.js

--- server/server.js
+++ server/server.js
@@ -6,12 +6,37 @@ import { connectDB } from "./lib/db.js";
 
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRouter.js";
-
+import { Server } from "socket.io";
 // create express app and HTTP server
 
 const app=express();
 const server=https.createServer(app);
 
+// Initialize socket.io server
+export const io= new Server(server, {
+	cors: {origin: "*"}
+})
+// Store online users
+export const userSocketMap= {};
+
+// Socket.io connections handler
+io.on("connection", ( socket)=>{
+	const userId= socket.handshake.query.userId;
+	console.log("USer Connected",userId );
+
+	if(userId)userSocketMap[userId]=socket.id;
+
+	//Emit online users to all connected clients
+	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+	socket.on("disconnect", ()=>{
+		console.log("USer Disconnected", userId);
+		delete userSocketMap[userId];
+		io.emit("getOnlineUsers", Object.keys(userSocketMap))
+	})
+
+
+})
 //Middleware
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
